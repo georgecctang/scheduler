bef075423406576e6041d63bd20a43421f9721b7
George Tang <georgecctang@gmail.com>
2020-10-26 19:15:04 -0400

    reactor Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,20 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = "button";
+   const { confirm, danger, disabled, onClick} = props;
+   const buttonClass = classNames('button', 
+   {
+      "button--confirm": confirm, 
+      "button--danger": danger
+   });
 
-   if (props.confirm) {
-      buttonClass += " button--confirm";
-   }
-
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
-
-
-   return <button disabled={props.disabled} onClick={props.onClick} className={buttonClass}>{props.children}</button>;
+   return <button 
+      disabled= {disabled}
+      onClick={onClick}
+      className={buttonClass}>
+      {props.children}
+   </button>;
 }
